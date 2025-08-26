35ca25bcd25a2dcf7ec17826f14e5c436d391362
David Sheldrick <d.j.sheldrick@gmail.com>
2025-08-26 09:23:59 +0100

    format

--- test/vitest.d.ts
+++ test/vitest.d.ts
@@ -2,6 +2,9 @@ import '@testing-library/jest-dom'
 import type * as matchers from '@testing-library/jest-dom/matchers'
 
 declare module 'vitest' {
-  interface Assertion<T = any> extends jest.Matchers<void, T>, matchers.TestingLibraryMatchers<T, void> {}
-  interface AsymmetricMatchersContaining extends matchers.TestingLibraryMatchers<any, any> {}
+  interface Assertion<T = any>
+    extends jest.Matchers<void, T>,
+      matchers.TestingLibraryMatchers<T, void> {}
+  interface AsymmetricMatchersContaining
+    extends matchers.TestingLibraryMatchers<any, any> {}
 }
